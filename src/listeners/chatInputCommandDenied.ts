018f96135acc1efeaba5deb878a24c5608460365
GoldenXLence <jack@koskie.ca>
2021-12-22 09:20:25 -0500

    feat: implement preconditions for slash commands

---
+++ src/listeners/chatInputCommandDenied.ts
@@ -0,0 +1,18 @@
+import { ChatInputCommandDeniedPayload, Command, Listener, UserError } from '@sapphire/framework';
+import { CommandInteraction, Interaction } from 'discord.js';
+import { VACClient } from '../lib';
+
+export class ChatInputCommandDeniedListener extends Listener {
+    public constructor(context: Listener.Context, options: Listener.Options) {
+        super(context, {
+            ...options,
+            once: false,
+            event: 'chatInputCommandDenied'
+        });
+    }
+
+    public run(error: UserError, payload: ChatInputCommandDeniedPayload) {
+        payload.interaction.reply(`${error.message} (Error: \`${error.identifier}\`)`);
+    }
+
+}
--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,10 +1,16 @@
 import { Precondition } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import type { Interaction, Message } from 'discord.js';
 
 export class OwnerOnlyPrecondition extends Precondition {
-    public run(message: Message) {
-        return message.author.id === process.env.OWNER_ID
+    public chatInputRun(interaction: Interaction) {
+        return interaction.user.id === process.env.OWNER_ID
             ? this.ok()
-            : this.error({ message: 'Only the bot owner can use this command!' });
+            : this.error({ message: 'Only the bot owner can use this command!', identifier: 'OWNER_ONLY' });
+    }
+}
+
+declare module '@sapphire/framework' {
+    interface Preconditions {
+        OwnerOnly: never;
     }
 }
